5404801c7678914179d3fc9e7c5896df8abe78ba
Christopher J. Brody <chris@brody.consulting>
2019-10-07 12:55:45 -0400

    squash-fix: script without backticks, *should* be able to work on Node.js 0.12

--- scripts/prepare-distcheck.js
+++ scripts/prepare-distcheck.js
@@ -2,42 +2,54 @@ const childProc = require('child_process')
 
 const fs = require('fs')
 
+const path = require('path')
+
 const copydir = require('copy-dir')
 
 const DISTCHECK_ROOT = 'distcheck'
 
-const DISTCHECK_BROWSER_ROOT = `${DISTCHECK_ROOT}/browser`
+const DISTCHECK_BROWSER_ROOT = path.join(DISTCHECK_ROOT, 'browser')
 
-const DISTCHECK_NODE_ROOT = `${DISTCHECK_ROOT}/node`
+const DISTCHECK_NODE_ROOT = path.join(DISTCHECK_ROOT, 'node')
 
 // I/O wrapper functions with logging:
 
 const commandSync = (c, o) => {
-  console.log(`* in ${o.cwd || process.cwd()} execute command: ${c}`)
+  console.log('* execure command in cwd:' + (o.cwd || process.cwd()))
+  console.log('  command: ' + c)
+
   // FUTURE TBD change to use `commandSync` from `execa`
-  // (once we drop support for some extra-old Node.js versions)
+  // in case support for some extra-old Node.js versions is removed
   childProc.execSync(c, o)
 }
 
 // XXX FUTURE TBD XXX
 const mkdirSync = (dir) => {
-  console.log(`* create directory ${dir}`)
+  console.log('* create directory: ' + dir)
+
   // XXX TBD XXX
   // FUTURE TBD use `ensureDirSync` from `fs-extra`
-  // (once we drop support for some extra-old Node.js versions)
+  // in case support for some extra-old Node.js versions is removed
   fs.mkdirSync(dir)
 }
 
 // FUTURE TBD simply use `copySync` from `fs-extra`
-// (once we drop support for some extra-old Node.js versions)
+// in case support for some extra-old Node.js versions is removed
 
 const copyDirSync = (src, dest) => {
-  console.log(`* copy artifacts directory from ${src} to ${dest}`)
+  console.log('* copy artifacts directory')
+  console.log('  from src: ' + src)
+  console.log('  to dest: ' + dest)
+
   copydir.sync(src, dest)
 }
 
 const copyFileSync = (src, dest) => {
-  console.log(`* copy file artifact from ${src} to ${dest}`)
+  console.log('* copy file artifact')
+  console.log('  from src: ' + src)
+  console.log('  to dest: ' + dest)
+
+  // workaround for missing `copyFileSync` on extra-old Node.js versions
   // fs.copyFileSync(src, dest)
   commandSync(
     `cp ${src} ${dest}`,
@@ -47,32 +59,36 @@ const copyFileSync = (src, dest) => {
 // This step is done by calling rimraf in `distcheck:prepare` script:
 // removeSync(DISTCHECK_ROOT)
 
-// Separate step needed since `recursive` option is not supported on
+// Separate step as a workaround since `recursive` option is not supported on
 // some old Node.js versions:
 mkdirSync(DISTCHECK_ROOT)
 
 mkdirSync(DISTCHECK_BROWSER_ROOT)
 
-copyDirSync('lib', `${DISTCHECK_BROWSER_ROOT}/src`)
-copyFileSync('lib/hrtime-browser.js', `${DISTCHECK_BROWSER_ROOT}/src/hrtime.js`)
-copyDirSync('test', `${DISTCHECK_BROWSER_ROOT}/test`)
-copyFileSync('karma.conf.js', `${DISTCHECK_BROWSER_ROOT}/karma.conf.js`)
+copyDirSync('lib', path.join(DISTCHECK_BROWSER_ROOT, 'src'))
+copyFileSync('lib/hrtime-browser.js', path.join(DISTCHECK_BROWSER_ROOT, 'src', 'hrtime.js'))
+copyDirSync('test', path.join(DISTCHECK_BROWSER_ROOT, 'test'))
+copyFileSync('karma.conf.js', path.join(DISTCHECK_BROWSER_ROOT, 'karma.conf.js'))
 
 commandSync('npm init -y', {
-  cwd: `${DISTCHECK_BROWSER_ROOT}`,
+  cwd: DISTCHECK_BROWSER_ROOT,
   stdio: 'inherit'
 })
 
-commandSync(`npm install babel-runtime@${require('../package.json').dependencies['babel-runtime']}`, {
-  cwd: `${DISTCHECK_BROWSER_ROOT}`,
-  stdio: 'inherit'
-})
+commandSync(
+  'npm install babel-runtime@' + require('../package.json').dependencies['babel-runtime'],
+  {
+    cwd: DISTCHECK_BROWSER_ROOT,
+    stdio: 'inherit'
+  })
 
-commandSync(`npm install babel-core@${require('../package.json').devDependencies['babel-core']}`, {
-  cwd: `${DISTCHECK_BROWSER_ROOT}`,
-  stdio: 'inherit'
-})
+commandSync(
+  'npm install babel-core@' + require('../package.json').devDependencies['babel-core'],
+  {
+    cwd: DISTCHECK_BROWSER_ROOT,
+    stdio: 'inherit'
+  })
 
 mkdirSync(DISTCHECK_NODE_ROOT)
-copyDirSync('lib', `${DISTCHECK_NODE_ROOT}/src`)
-copyDirSync('test', `${DISTCHECK_NODE_ROOT}/test`)
+copyDirSync('lib', path.join(DISTCHECK_NODE_ROOT, 'src'))
+copyDirSync('test', path.join(DISTCHECK_NODE_ROOT, 'test'))
